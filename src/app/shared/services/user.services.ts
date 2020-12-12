5a6442357c98235facbd545dbbd3ad1245f3fcad
abakarabdelsalam <abdelsalamabakar3@gmail.com>
2020-12-12 21:00:52 +0100

    projet5

---
+++ src/app/shared/services/user.services.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable()
+export class UserService {
+  public users: BehaviorSubject<string[]> = new BehaviorSubject<string[]>([]);
+
+  constructor() { }
+
+  addUser(user: string) {
+    this.users.value.push(user);
+  }
+}
--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../shared/services/user.services';
 
 @Component({
   selector: 'app-user-list',
@@ -8,11 +9,12 @@ import { Component, OnInit } from '@angular/core';
 export class UserListComponent implements OnInit {
   public users: string[];
 
-  constructor(
-    // il faut probablement injecter un service ici !
-  ) { }
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
+    this.userService.users.subscribe((users: string[])=> {
+      this.users=users
+    });
     // il faut initialiser les users ici avec le service
   }
 
